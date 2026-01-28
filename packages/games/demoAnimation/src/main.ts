ff19b0dcc113c04dfac50348235d7f403d5d7eae
JT Smith <jt@plainblack.com>
2026-01-28 16:19:57 -0600

    removing a whole bunch of code that should have been deleted long ago

--- packages/games/demoAnimation/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue';
-import { App } from './ui/index.js';
-
-const app = createApp(App);
-app.mount('#app');
--- packages/games/demoAnimation/src/ui/index.ts
+++
@@ -1,3 +0,0 @@
-import App from './App.vue';
-export { App };
-export { default as GameTable } from './components/GameTable.vue';
